6c32a8d8befd23a73a5adcc9e43e6359e03dae4e
Matt <mattray0295@gmail.com>
2017-11-27 11:42:59 -0600

    setup react-app & express server for api

---
+++ api-server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const axios = require('axios');
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Log HTTP Requests
+app.use(morgan('combined'))
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+
+
+
+app.get('/food/:q', (req, res) => {
+  let query = req.params.q;
+  const url = `https://trackapi.nutritionix.com/v2/search/instant?query=${query}`;
+  axios.get(url, {
+    headers: {
+      'x-app-id': process.env.NUTRITION_APP_ID,
+      'x-app-key': process.env.NUTRITION_APP_KEY,
+      'x-remote-user-id': process.env.REMOTE_USER_ID
+    }
+  })
+  .then((response) => {
+    res.status(200).send(response.data.common);
+  })
+  .catch((err) => {
+    res.status(400).send('Error:', err);
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// App Start
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error: ${err}`);
+  } else {
+    console.log(`App running on port ${port}`);
+  }
+});
