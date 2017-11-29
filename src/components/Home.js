9a91128838f5d751d1acbbe303c999a74358cfe6
Matt <mattray0295@gmail.com>
2017-11-29 11:46:32 -0600

    styling for phone number form

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,27 +19,30 @@ class Home extends Component {
 
   _getPhoneNumber(e) {
     e.preventDefault();
-    // console.log(this.phoneNumber.value)
     axios.get(`/api/twilio/${this.phoneNumber.value}`)
     .then((res) => {
-      console.log(res);
+      console.log('SMS sent successfully.');
     })
-
-
+    this.phoneNumber.value = ''
   }
-
-
-
 
   render() {
     return (
-      <div>
+      <div className="home">
         <h1>Login and track your macros!</h1>
+        <h1>Enter your phone number to get fitness updates</h1>
         <form onSubmit={this._getPhoneNumber}>
-          <h1>Enter your phone number to get fitness updates</h1>
-          <input type="text" ref={(input) => this.phoneNumber = input}/>
-          <input type="submit" value="submit" />
-        </form>
+          <div id="input_wrapper">
+            <p>
+              <label htmlFor="login">Phone Number</label>
+              <i className="icon icon-user icon-large fa-phone"></i>
+              <input type="text" name="login" id="login" placeholder="Phone Number" ref={(input) => this.phoneNumber = input} />
+            </p>
+          </div>
+            <p>
+              <input type="submit" id="log-in" name="log-in" value="Submit" />
+            </p>
+          </form>
       </div>
     );
   }
