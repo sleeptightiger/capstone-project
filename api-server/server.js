0cd68d1b117881846a0318ed15f0da71945addfc
Matt <mattray0295@gmail.com>
2017-11-28 23:38:36 -0600

    added more cors headers

--- api-server/server.js
+++ api-server/server.js
@@ -13,7 +13,8 @@ app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*',);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
